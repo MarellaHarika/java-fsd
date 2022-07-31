2df9f59c5c390a93e316107675fec6559c56f889
Harika <marellaharika01@gmail.com>
2022-07-31 19:23:29 +0530

    changes

--- phase1-assistedpractice1A/src/synchronization/sync.java
+++ phase1-assistedpractice1A/src/synchronization/sync.java
@@ -1,5 +1,17 @@
 package synchronization;
 
 public class sync {
+	public static void main(String[] args) {
+		
+		Sender sender = new  Sender();
+		
+		User t1= new User("harika", "Hello Good morning....!", sender);
+		User t2= new User("marella","Hii!! how  are you?",sender);
+		
+		t1.start();
+		t2.start();
+	}
+
+
 
 }
---
+++ phase1-assistedpractice1A/src/tryCatch.java
@@ -0,0 +1,19 @@
+public class tryCatch
+{
+    public static void main(String args[]) 
+    {
+        int[] array = new int[3];
+        try 
+        {
+            array[5] = 2;
+        }
+        catch (ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Array index is out of bounds!"); 
+        }
+        finally 
+        {
+            System.out.println("The array size is : " + array.length);
+        }
+    }
+}
