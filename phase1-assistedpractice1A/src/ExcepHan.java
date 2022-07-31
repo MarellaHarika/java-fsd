128cb27e57015b71bcf4f69bce9c08533c1aac81
Harika <marellaharika01@gmail.com>
2022-07-31 19:32:22 +0530

    changes

---
+++ phase1-assistedpractice1A/src/ExcepHan.java
@@ -0,0 +1,15 @@
+public class ExcepHan {
+	public static void main(String[] args) {
+		
+			try{
+				System.out.println(5/0);
+				
+			}
+			catch(ArithmeticException e){
+				e.printStackTrace();
+			}
+			System.out.println("Hello World");
+	}
+
+
+}
